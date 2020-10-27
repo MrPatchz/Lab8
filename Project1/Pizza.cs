6168fbcc7ea8f2928af00eb8ec0a32822e17e3d9
Jacques Zwielich <moshhowl@gmail.com>
2020-10-26 20:50:03 -0700

    Add project files.

---
+++ Project1/Pizza.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    class Pizza
+    {
+        private string size;
+        private int toppingCheese;
+        private int toppingPep;
+        private int toppingHam;
+
+        public Pizza(string size, int toppingCheese, int toppingPep, int toppingHam)
+        {
+            this.size = size;
+            this.toppingCheese = toppingCheese;
+            this.toppingHam = toppingHam;
+            this.toppingPep = toppingPep;
+        }
+
+        public string getSize()
+        {
+            return this.size;
+        }
+
+        public int getToppingCheese()
+        {
+            return this.toppingCheese;
+        }
+
+        public int getToppingPep()
+        {
+            return this.toppingPep;
+        }
+
+        public int getToppingHam()
+        {
+            return this.toppingHam;
+        }
+
+        public void setSize(string size)
+        {
+            this.size = size;
+        }
+
+        public void setToppingCheese(int toppingCheese)
+        {
+            this.toppingCheese = toppingCheese;
+        }
+
+        public void setToppingPep(int toppingPep)
+        {
+            this.toppingPep = toppingPep;
+        }
+
+        public void setToppingHam(int toppingHam)
+        {
+            this.toppingHam = toppingHam;
+        }
+
+        public double CalculateCost()
+        {
+            double cost = 0.0;
+            if (this.size == "small")
+            {
+                cost += 10;
+            }
+            else if (this.size == "medium")
+            {
+                cost += 12;
+            }
+            else if (this.size == "large")
+            {
+                cost += 14;
+            }
+            else
+            {
+                cost = 0;
+            }
+            return (cost + (toppingPep * 2) + (toppingHam * 2) + (toppingCheese * 2));
+        }
+
+        public string ToString()
+        {
+            return ("The " + size + " Pizza with " +
+                toppingCheese + " cheese toppings, " +
+                toppingHam + " ham toppings and " +
+                toppingPep + " pepperoni toppings costs $" +
+                CalculateCost());
+        }
+    }
+}
+    
---
+++ Project1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizza bigDaddy = new Pizza("small", 2, 2, 2);
+            Console.Out.WriteLine("~~~~~~~~~~~~Initialize Pizza~~~~~~~~~~~~~~~~~");
+            Console.Out.WriteLine(bigDaddy.ToString());
+            Console.Out.WriteLine("~~~~~~~~~~~~Getters and setters~~~~~~~~~~~~~~~~~");
+            bigDaddy.setSize("large");
+            bigDaddy.setToppingCheese(10);
+            bigDaddy.setToppingPep(10);
+            bigDaddy.setToppingHam(10);
+            Console.Out.WriteLine(bigDaddy.getSize());
+            Console.Out.WriteLine(bigDaddy.getToppingCheese());
+            Console.Out.WriteLine(bigDaddy.getToppingPep());
+            Console.Out.WriteLine(bigDaddy.getToppingHam());
+            Console.Out.WriteLine("~~~~~~~~~~~~Final ToString~~~~~~~~~~~~~~~~~");
+            Console.Out.WriteLine(bigDaddy.ToString());
+        }
+    }
+}
